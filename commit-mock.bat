c02a352f3e9cb5495f1c03cc4488506d7ddb4294
Divya jangir <jangiddivya@834>
2026-04-27 02:31:39 +0530

    Deploy with Mock API for GitHub Pages

---
+++ commit-mock.bat
@@ -0,0 +1,5 @@
+@echo off
+cd C:\Users\motir\Downloads\tasteful-deploy
+git add -A
+git commit -m "Deploy with Mock API for GitHub Pages"
+git push origin gh-pages
---
+++ commit.bat
@@ -0,0 +1,4 @@
+@echo off
+cd C:\Users\motir\Downloads\tasteful-deploy
+git commit -m "Add 404 for routing fallback"
+git push origin gh-pages
